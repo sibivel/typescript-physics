c38b7843d465756d2329024ad7dd2370e0b8cf80
sibi <sibivel979@gmail.com>
2021-09-14 22:07:35 -0400

    add css

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import "./styles.css";
 window.addEventListener('load', () => {
   const header = document.createElement('h1');
-  header.innerText = 'Webpack❤️TS Header'
 
   const body = document.querySelector('body');
   body.prepend(header);
@@ -8,6 +8,6 @@ window.addEventListener('load', () => {
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-  ctx.fillStyle = 'cyan'
+  ctx.fillStyle = 'gray'
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 })
